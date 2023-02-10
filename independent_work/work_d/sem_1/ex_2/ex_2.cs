3d590faa09a8f63b41807e465deda3ba9ad343d6
Berouzze <dmitriyshishoff@gmail.com>
2023-02-10 20:57:51 +0300

    Решил вторуюю задачу

---
+++ independent_work/work_d/sem_1/ex_2/ex_2.cs
@@ -0,0 +1,9 @@
+﻿// 2. Напишите программу, которая на вход принимает число и выдает его квадрат (число умноженное на само себя).
+
+Console.WriteLine("Введите целое число: ");
+
+int number_one = Convert.ToInt32(Console.ReadLine());
+
+number_one = number_one * number_one;
+
+Console.WriteLine(number_one);
