60b37f3bc419ce51db3262cd3687a7785c206dcb
Berouzze <dmitriyshishoff@gmail.com>
2023-02-10 20:51:26 +0300

    Решил первую задачу

---
+++ independent_work/work_d/sem_1/ex_1/ex_1.cs
@@ -0,0 +1,9 @@
+﻿// 1. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите целое трехзначное число: ");
+
+int number_one = Convert.ToInt32(Console.ReadLine());
+
+number_one = (number_one/10)%10;
+
+Console.WriteLine(number_one + " вторая цифра вашего числа.");
