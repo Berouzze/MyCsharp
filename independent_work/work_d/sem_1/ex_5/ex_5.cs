3f2fbcdfaa447ad5d37df3a0575dfdf05f3aad42
Berouzze <dmitriyshishoff@gmail.com>
2023-02-11 11:59:46 +0300

    Решил пятую задачу

---
+++ independent_work/work_d/sem_1/ex_5/ex_5.cs
@@ -0,0 +1,10 @@
+﻿// 5. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите свое целое положительное число: ");
+
+int number_one = Convert.ToInt32(Console.ReadLine());
+int size = -(number_one);
+while (size <= number_one) {
+    Console.WriteLine(size);
+    size++;
+}
