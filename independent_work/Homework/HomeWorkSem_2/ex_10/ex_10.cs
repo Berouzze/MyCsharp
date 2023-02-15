b23cc640686c552963093072b3140296cd8d15f3
Berouzze <dmitriyshishoff@gmail.com>
2023-02-15 11:11:31 +0300

    Добавил решение и к отрицательным числам домашней задачи второго семинара №10

--- independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
+++ independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
@@ -6,7 +6,7 @@ Console.WriteLine("Введите целое трехзначное число: 
 int number = Convert.ToInt32(Console.ReadLine());
 int secondNumb = 0;
 
-if (number > 99 && number < 1000) 
+if (number > 99 && number < 1000 || number<-99 && number > -1000) 
 {
      
      secondNumb = (number/10)%10;
