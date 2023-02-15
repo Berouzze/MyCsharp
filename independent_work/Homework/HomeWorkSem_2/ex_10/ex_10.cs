e87d7a39c3b26bfaa83460c15b54f5b036e9238b
Berouzze <dmitriyshishoff@gmail.com>
2023-02-15 10:52:18 +0300

    Решил домашнюю задачу второго семинара №10

---
+++ independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное
+ число и на выходе показывает вторую цифру этого числа. */
+
+Console.WriteLine("Введите целое трехзначное число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int secondNumb = 0;
+
+if (number > 99 && number < 1000) 
+{
+     
+     secondNumb = (number/10)%10;
+    
+    Console.Write(secondNumb);
+} 
+else 
+{
+    Console.WriteLine("Введите целое трехзначное число и попробуйте еще раз!");
+}
