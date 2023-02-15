d373a070968c9ab4ebb1aac16907c44ea3bd8ab2
Berouzze <dmitriyshishoff@gmail.com>
2023-02-15 11:20:41 +0300

    Решил домашнюю задачу второго семинара №13

--- independent_work/Homework/HomeWorkSem_2/ex_13/ex_13.cs
+++ independent_work/Homework/HomeWorkSem_2/ex_13/ex_13.cs
@@ -1,12 +1,17 @@
 ﻿/* Задача 13: Напишите программу, которая выводит третью 
 цифру заданного числа или сообщает, что третьей цифры нет. */
 
-   Console.Write("Введи число: ");
-int anyNumber = Convert.ToInt32(Console.ReadLine());
-string anyNumberText = Convert.ToString(anyNumber);
-if (anyNumberText.Length > 2){
-  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+Console.Write("Введи целове число: ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+int thirdNumber = 0;
+
+if (number>99 || number<-99) 
+{
+    thirdNumber = (number / 100)%10;
+    Console.Write(thirdNumber);
 }
-else {
-  Console.WriteLine("-> третьей цифры нет");
-}    
+else 
+{
+    Console.Write("Третья цифра отсутствует");
+}
