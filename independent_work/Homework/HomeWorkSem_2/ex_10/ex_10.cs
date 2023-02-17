97a136e714f5a21bb96c05972878d5975b793734
Berouzze <dmitriyshishoff@gmail.com>
2023-02-17 16:12:13 +0300

    Решил задачу с палидромом используя методы

--- independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
+++ independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
@@ -5,8 +5,7 @@ Console.WriteLine("Введите целое трехзначное число: 
 
 int number = Convert.ToInt32(Console.ReadLine());
 int secondNumb = 0;
-int myNumber(int y)
-{
+int myNumber(int y) {
 return (y/10)%10;
 }
 if (number > 99 && number < 1000 || number<-99 && number > -1000) 
---
+++ independent_work/library/library.cs
@@ -0,0 +1,9 @@
+public class library {
+
+
+int GetNumber(string text)
+{
+ System.Console.WriteLine(text);
+ return Convert.ToInt32(Console.ReadLine());
+}
+}
