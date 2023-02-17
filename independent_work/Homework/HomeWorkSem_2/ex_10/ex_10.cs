a27f413c187b8c175501ae567af75f66f9fd2d40
Berouzze <dmitriyshishoff@gmail.com>
2023-02-17 10:44:39 +0300

    Использовал метод и возрат значения в задачн №10 2-го семинара

--- independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
+++ independent_work/Homework/HomeWorkSem_2/ex_10/ex_10.cs
@@ -5,11 +5,13 @@ Console.WriteLine("Введите целое трехзначное число: 
 
 int number = Convert.ToInt32(Console.ReadLine());
 int secondNumb = 0;
-
+int myNumber(int y) {
+return (y/10)%10;
+}
 if (number > 99 && number < 1000 || number<-99 && number > -1000) 
 {
      
-     secondNumb = (number/10)%10;
+     secondNumb = myNumber(number);
     
     Console.Write(secondNumb);
 } 
