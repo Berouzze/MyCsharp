2c2438078cf20d29f8a8b71a7ecee49b7cecd36b
Berouzze <dmitriyshishoff@gmail.com>
2023-03-06 14:33:30 +0300

    complete task_50

--- independent_work/Homework/HomeWorkSem_7/ex_47/ex_47.cs
+++ independent_work/Homework/HomeWorkSem_7/ex_47/ex_47.cs
@@ -5,7 +5,7 @@
 // 1 -3,3 8 -9,9
 // 8 7,8 -7,1 9
 
- // Ограничил диапазон рандомных чисел от -100 до 101 для удобства
+// Ограничил диапазон рандомных чисел от -100 до 101 для удобства
 
 // Ввод числа
 int GetNumber(string text)
@@ -22,16 +22,16 @@ double[,] CreateArray(int size, int size2)
 // Заполнение массива рандомными  вещественными числами
 void FillByRand(double[,] array)
 {
-    
-    for (int index = 0;index < array.GetLength(0); index++)
+
+    for (int index = 0; index < array.GetLength(0); index++)
     {
-        for  (int index2 = 0; index2 < array.GetLength(1); index2++)
+        for (int index2 = 0; index2 < array.GetLength(1); index2++)
         {
             //Создание объекта для генерации чисел
             array[index, index2] = new Random().Next(-100, 101);
 
         }
-    
+
     }
 
 }
@@ -44,13 +44,12 @@ void PrintArray(double[,] array)
         {
             Console.Write($" {array[index, index2]} ");
         }
-         Console.WriteLine();
+        Console.WriteLine();
     }
 }
 
-    int sizeM = GetNumber("Задайте двумерный массив размером m на n, m: ");
-    int sizeN = GetNumber("n: ");
-    double[,] numbers = CreateArray(sizeM, sizeN);
-    FillByRandPrint(numbers);
-    PrintArray(numbers);
-   
+int sizeM = GetNumber("Задайте двумерный массив размером m на n, m: ");
+int sizeN = GetNumber("n: ");
+double[,] numbers = CreateArray(sizeM, sizeN);
+FillByRand(numbers);
+PrintArray(numbers);
---
+++ independent_work/Homework/HomeWorkSem_7/ex_50/ex_50.cs
@@ -0,0 +1,71 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции
+// элемента в двумерном массиве, и возвращает значение этого
+// элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1, 7 -> такого числа в массиве нет
+
+// Ввод числа
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+// Создание двухмерного массива 
+double[,] CreateArray(int size, int size2)
+{
+    return new double[size, size2];
+}
+
+// Заполнение массива рандомными  вещественными числами
+void FillByRand(double[,] array)
+{
+
+    for (int index = 0; index < array.GetLength(0); index++)
+    {
+        for (int index2 = 0; index2 < array.GetLength(1); index2++)
+        {
+            //Создание объекта для генерации чисел
+            array[index, index2] = new Random().Next(-100, 101);
+
+        }
+
+    }
+
+}
+//Печать массива  ---- для проверки
+// void PrintArray(double[,] array)
+// {
+//     for (int index = 0; index < array.GetLength(0); index++)
+//     {
+//         for (int index2 = 0; index2 < array.GetLength(1); index2++)
+//         {
+//             Console.Write($" {array[index, index2]} ");
+//         }
+//          Console.WriteLine();
+//     }
+// }
+
+void SearchArrayElements(double[,] array, int firstPos, int secondPos)
+{
+    if (firstPos < 0 | firstPos > array.GetLength(0) - 1 | secondPos < 0 | secondPos > array.GetLength(1) - 1)
+    {
+        Console.WriteLine("Элемент не существует");
+    }
+    else
+    {
+        Console.WriteLine("Значение элемента массива = {0}", array[firstPos, secondPos]);
+    }
+}
+int sizeM = new Random().Next(1, 10);
+int sizeN = new Random().Next(1, 10);
+double[,] numbers = CreateArray(sizeM, sizeN);
+int firstPosition = GetNumber("Номер строки: ");
+int secondPosition = GetNumber("Номер столбца: ");
+FillByRand(numbers);
+// PrintArray(numbers);  //------ для проверки
+SearchArrayElements(numbers, firstPosition, secondPosition);
