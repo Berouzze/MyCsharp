f89243044e986c4b50eb19476c1d340da085adbb
Berouzze <dmitriyshishoff@gmail.com>
2023-03-12 11:55:09 +0300

    complete task_64

---
+++ independent_work/Homework/HomeWorkSem_9/ex_64/ex_64.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64: Задайте значение N. Напишите программу,которая выведет
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+int GetNumber(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintNumbers(int numb)
+{
+    if (numb < 0) { System.Console.WriteLine("Не натуральное число"); } 
+    if ( numb == 0) { return; }
+    Console.Write($"{numb} ");
+    PrintNumbers(numb - 1);
+}
+
+
+int number = GetNumber("Введите число N: ");
+PrintNumbers(number);
